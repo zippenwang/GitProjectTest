9c91396b043aaf903d1298b3dd7064681f536da0
zippenwang <zippenwang@outlook.com>
2018-03-30 10:56:11 +0800

    Random Test

--- src/com/wzp/test/gitproject/Test3.java
+++ src/com/wzp/test/gitproject/Test3.java
@@ -1,9 +1,33 @@
 package com.wzp.test.gitproject;
 
+import java.util.Random;
+
 public class Test3 {
 
 	public static void main(String[] args) {
+		
+		
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				Random random = new Random(123456);
+				
+				System.out.println(Thread.currentThread() + "---" + random.nextInt());
+				System.out.println(Thread.currentThread() + "---" + random.nextInt());
+				System.out.println(Thread.currentThread() + "---" + random.nextInt());
+			}
+		}).start();
+		
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				Random random = new Random(123456);
 
+				System.out.println(Thread.currentThread() + "---" + random.nextInt());
+				System.out.println(Thread.currentThread() + "---" + random.nextInt());
+				System.out.println(Thread.currentThread() + "---" + random.nextInt());
+			}
+		}).start();
 	}
 
 }
